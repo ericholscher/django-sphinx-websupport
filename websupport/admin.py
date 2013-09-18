637558b71e18981951f1337e66a6ec8948b2c815
Eric Holscher <eric@ericholscher.com>
2013-09-18 16:21:09 -0700

    Fix admin

--- websupport/admin.py
+++ websupport/admin.py
@@ -4,9 +4,11 @@ from .models import SphinxComment, SphinxNode
 class SphinxNodeAdmin(admin.ModelAdmin):
     search_fields = ('id', 'document')
     list_filter = ('project__name',)
+    raw_id_fields = ('project', 'version')
 
 class SphinxCommentAdmin(admin.ModelAdmin):
     search_fields = ('text',)
+    raw_id_fields = ('node',)
 
 admin.site.register(SphinxNode, SphinxNodeAdmin)
 admin.site.register(SphinxComment, SphinxCommentAdmin)
